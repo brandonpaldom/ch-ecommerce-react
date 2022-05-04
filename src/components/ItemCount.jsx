dec2479ea1c6c9f986b5add7f988b47d9b2b6222
Brandon Palmeros <brandonpaldom@gmail.com>
2022-05-03 21:48:28 -0500

    Handler functions have been added

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -5,11 +5,19 @@ import PlusIcon from '../assets/icons/plus-small.svg';
 function ItemCount({ stock, initial }) {
   const [count, setCount] = useState(initial);
 
+  function onAdd() {
+    setCount(count + 1);
+  }
+
+  function onRemove() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="flex w-[240px] flex-col items-center gap-2">
       <div className="flex w-full items-center justify-between rounded-lg border border-yellow-500 p-1">
         <button
-          onClick={() => setCount(count - 1)}
+          onClick={onRemove}
           className={`${count <= initial ? 'opacity-10' : null}`}
           disabled={count <= initial}
         >
@@ -17,7 +25,7 @@ function ItemCount({ stock, initial }) {
         </button>
         <span className="text-center font-semibold">{count}</span>
         <button
-          onClick={() => setCount(count + 1)}
+          onClick={onAdd}
           className={`${count >= stock ? 'opacity-10' : null}`}
           disabled={count >= stock}
         >
