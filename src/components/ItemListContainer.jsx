705e367fa7a74b7168c05dc3d69e67a0960ee6eb
Brandon Palmeros <brandonpaldom@gmail.com>
2022-05-15 12:38:35 -0500

    Fixed center elements

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -33,14 +33,16 @@ function ItemListContainer() {
     <div className="p-6">
       <ItemList items={products} loading={loading} />
       {products.length === 0 && (
-        <div className="grid h-[320px] w-full grid-cols-1 place-content-center gap-4">
-          <p className="text-neutral-500">
-            Lo sentimos, no hay productos en esta categoría en este momento. Por
-            favor, vuelva a comprobarlo pronto.
-          </p>
-          <Link to="/" className="w-max bg-black/10 p-2">
-            Regresar al inicio
-          </Link>
+        <div className="grid h-[320px] w-full grid-cols-1 place-content-center">
+          <div className="flex flex-col items-center gap-4">
+            <p className="text-center text-neutral-500">
+              Lo sentimos, no hay productos en esta categoría en este momento.
+              Por favor, vuelva a comprobarlo pronto.
+            </p>
+            <Link to="/" className="w-max bg-black/10 p-2">
+              Regresar al inicio
+            </Link>
+          </div>
         </div>
       )}
     </div>
