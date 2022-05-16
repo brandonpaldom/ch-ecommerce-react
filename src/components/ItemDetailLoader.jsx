0ee11c085b289236feb586d1a0073ad231491b79
Brandon Palmeros <brandonpaldom@gmail.com>
2022-05-16 11:51:58 -0500

    Added logo to the loader

--- src/components/ItemDetailLoader.jsx
+++ src/components/ItemDetailLoader.jsx
@@ -1,7 +1,11 @@
+import LogoLoader from '../assets/magnolia-loader.svg';
+
 function ItemDetailLoader() {
   return (
     <div className="mx-auto grid max-w-[1024px] animate-pulse grid-cols-1 p-6 sm:grid-cols-2">
-      <div className="aspect-[3/4] w-full rounded bg-neutral-200" />
+      <div className="flex aspect-[3/4] w-full flex-col items-center justify-center rounded bg-neutral-200">
+        <img src={LogoLoader} alt="" />
+      </div>
       <div className="flex flex-col gap-4 bg-white p-6">
         <div className="h-8 w-2/3 rounded bg-neutral-200" />
         <div className="h-8 w-1/4 rounded bg-neutral-200" />
--- src/components/ItemLoader.jsx
+++ src/components/ItemLoader.jsx
@@ -1,7 +1,11 @@
+import LogoLoader from '../assets/magnolia-loader.svg';
+
 function ItemLoader() {
   return (
     <div className="flex animate-pulse flex-col gap-2">
-      <div className="aspect-[3/4] w-full rounded bg-neutral-200" />
+      <div className="flex aspect-[3/4] w-full flex-col items-center justify-center rounded bg-neutral-200">
+        <img src={LogoLoader} alt="" />
+      </div>
       <div className="h-4 w-2/3 rounded bg-neutral-200" />
       <div className="h-4 w-1/3 rounded bg-neutral-200" />
     </div>
