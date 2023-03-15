2313955844208ab97e0f4bfb91cd582b9715f047
Brandon Palmeros <brandonpaldom@gmail.com>
2023-03-15 17:00:33 -0600

    Font changed to DM Sans

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['DM Sans', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/forms', '@tailwindcss/line-clamp')],
